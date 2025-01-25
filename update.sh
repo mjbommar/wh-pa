178d5948373503e9d483d31e7265a48da92c16ee
Mike Bommarito <michael.bommarito@gmail.com>
2025-01-25 18:51:32 -0500

    auto-push

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+# python
 export PYTHONPATH=.
 uv run python3 src/downloader.py
 uv run python3 src/converter.py
 uv run python3 src/summarizer.py
+
+# git
+git add hashes.json data/
+git commit -a -m "auto-push"
+git push
+
 date
